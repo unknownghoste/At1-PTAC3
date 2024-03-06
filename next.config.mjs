ce46fd2bd1c28ed18049ec8277233a15c6963530
unknownghoste <99215178+unknownghoste@users.noreply.github.com>
2024-03-05 21:01:44 -0400

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+    images: {
+        domains: [
+            "www.ifms.edu.br",
+            "www.ifms.ead.edu.br"
+        ]
+    }
+};
+
+export default nextConfig;
