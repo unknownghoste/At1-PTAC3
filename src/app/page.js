32a21c8cbf32ee9dbea67bae184df5d9f813630e
unknownghoste <99215178+unknownghoste@users.noreply.github.com>
2024-03-20 14:03:44 -0400

    page.js updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,23 @@
+const url = "https://back-end-ifms-seven.vercel.app/";
 
-export default function Home() {
-  return (
+export default async function Home() {
+  const resposta = await fetch(url, {
+    next: {
+      revalidate: 1
+    }
+  });
+  const campi = await resposta.json();
+
+  return( 
     <main>
-     
+      <h1> Home </h1>
+      {campi.map((campus) => 
+      
+      <div>
+        <p>{campus.nome_campi}</p>
+      </div>
+      )
+      }
     </main>
-  );
+  ) 
 }
